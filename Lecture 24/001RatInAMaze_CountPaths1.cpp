26ccef71259022f65950defdc041bc699d931046
abhishek18124 <abhishek18124@iiitd.ac.in>
2024-11-02 17:10:37 +0530

    Lecture 24

---
+++ Lecture 24/001RatInAMaze_CountPaths1.cpp
@@ -0,0 +1,89 @@
+/*
+
+Given a m x n maze with some blocked cells (marked with 'X'), and rat that wants to
+go from the src (0, 0) to the dst (m-1, n-1) design a recursive algorithm to
+
+	> check if there exists a path from src to dst
+
+such at each step the rat can only move in the right or down direction.
+
+Constraint
+
+ 1 <= m, n < 10
+
+Example
+	Input :
+
+		maze = {"0000",
+	            "00X0",
+	            "000X",
+	            "0X00"}
+
+	Output :
+
+		true (Yes, a path exist from src to dst)
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int cnt = 0;
+
+bool f(char maze[][10], int m, int n, int i, int j) {
+
+	// base case
+
+	if (i == m || j == n) { // you've gone outside the maze
+		return false;
+	}
+
+	if (maze[i][j] == 'X') { // you've reached a blocked cell
+		return false;
+	}
+
+	if (i == m - 1 and j == n - 1) { // you've reached the destn and it is not blocked
+		cnt++; // you've found a path
+		return true;
+	}
+
+	// recursive case
+
+	// f(i, j) = check if there exists a path from the (i, j)th cell to the (m-1, n-1)th cell
+
+	// decide the next step
+
+	// option 1 : move right
+
+	bool X = f(maze, m, n, i, j + 1);
+
+	// option 2 : move down
+
+	bool Y = f(maze, m, n, i + 1, j);
+
+	return X or Y;
+
+	// return f(maze, m, n, i, j + 1) or f(maze, m, n, i + 1, j);
+
+}
+
+int main() {
+
+	char maze[][10] = {
+		"0000",
+		"00X0",
+		"000X",
+		"0X00"
+	};
+
+	int m = 4;
+	int n = 4;
+
+	f(maze, m, n, 0, 0) ? cout << "path exists" << endl :
+	                           cout << "path doesn't exists" << endl;
+
+	cout << cnt << endl;
+
+	return 0;
+}
---
+++ Lecture 24/005Sudoko.cpp
@@ -0,0 +1,116 @@
+/*
+
+Sudoku
+
+Given a partially filled 'n x n' grid, the goal is to assign digits from 1 to 9 to the empty cells
+so that every row, column, and subgrid of size '√n x √n'  contains exactly one instance of
+the digits from 1 to 9.
+
+Assume 'n' is a perfect square, hence there are 'n' subgrids of size '√n x √n' each.
+
+
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+bool isValid(int grid[][9], int n, int i, int j, int d) {
+
+	for (int k = 0; k < n; k++) {
+		if (grid[i][k] == d || grid[k][j] == d) {
+			return false;
+		}
+	}
+
+	// digit 'd' is not is the same row or coln but it can be in the same subgrid of the (i, j)th cell
+
+	// check if 'd' is in the subgrid of the (i, j)th cell ?
+
+	int rn = sqrt(n);
+
+	int sx = i - (i % rn);
+	int sy = j - (j % rn);
+
+	for (int x = sx; x < sx + rn; x++) {
+		for (int y = sy; y < sy + rn; y++) {
+			if (grid[x][y] == d) {
+				return false;
+			}
+		}
+	}
+
+	return true; // d is a valid digit i.e. it can be assigned to the i,jth cell of the grid
+
+}
+
+void solveSudoku(int grid[][9], int n, int i, int j) {
+
+	// base case
+	if (i == n) {
+		// all empty cells have been assigned valid digits
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << grid[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+		return;
+	}
+
+	// recursive case
+
+	if (j == n) {
+		// digits have been assigned to all the cells in the ith row -
+		// move to the next row, and start filling from its 0th cell
+		solveSudoku(grid, n, i + 1, 0);
+		return;
+	}
+
+	if (grid[i][j] != 0) {
+		// (i, j)th cell is already assigned a digit -
+		// move to the next cell at the (i, j+1)th idx
+		solveSudoku(grid, n, i, j + 1);
+		return;
+	}
+
+	// assign a valid digit 'd' to the (i, j)th cell where 1<=d<=9
+	for (int d = 1; d <= 9; d++) {
+		if (isValid(grid, n, i, j, d)) {
+			grid[i][j] = d;
+			solveSudoku(grid, n, i, j + 1);
+			grid[i][j] = 0; // backtracking
+		}
+	}
+
+}
+
+int main() {
+
+	int n = 9;
+	int grid[][9] = {
+		{3, 0, 6, 5, 0, 8, 4, 0, 0},
+		{5, 2, 0, 0, 0, 0, 0, 0, 0},
+		{0, 8, 7, 0, 0, 0, 0, 3, 1},
+		{0, 0, 3, 0, 1, 0, 0, 8, 0},
+		{9, 0, 0, 8, 6, 3, 0, 0, 5},
+		{0, 5, 0, 0, 9, 0, 6, 0, 0},
+		{1, 3, 0, 0, 0, 0, 2, 5, 0},
+		{0, 0, 0, 0, 0, 0, 0, 7, 4},
+		{0, 0, 5, 2, 0, 6, 3, 0, 0}
+	};
+
+	solveSudoku(grid, n, 0, 0);
+
+	// for (int i = 0; i < n; i++) {
+	// 	for (int j = 0; j < n; j++) {
+	// 		cout << grid[i][j] << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+	// cout << endl;
+
+	return 0;
+}
